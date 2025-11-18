45730cddaf06b88cb54f9e8d28cc395eccbec00a
Vu Minh Nhat <74690179+NhatUET@users.noreply.github.com>
2025-11-18 18:32:59 +0700

    www

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "josephAI",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
+    role: "AI Engineer",
     bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    email: "minhnhat8dc@gmail.com",
+    linkedin: "nhatvujr84",
+    github: "NhatUET",
     instagram: "",
   },
   projects: [
     {
-      name: `morethan-log`,
+      name: `josephAI-blog`,
       href: "https://github.com/morethanmin/morethan-log",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "josephAI-blog",
+    description: "welcome to josephAI-blog!",
     scheme: "dark", // 'light' | 'dark' | 'system'
   },
 
